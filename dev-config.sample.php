df03dea8e6fa5ffff40a2ac699825c5c0f9867f9
ibenot <benjamin.notteghem@gmail.com>
2016-06-07 14:36:38 +0200

    Installation script

--- dev-config.sample.php
+++ dev-config.sample.php
@@ -13,8 +13,6 @@ define('DB_PASSWORD', 'XXXXXXXXXXXXXXXXX');
 define('DB_HOST', 'XXXXXXXXXXXXXXXXX');
 
 ini_set( 'display_errors', E_ALL );
-define( 'WP_DEBUG_DISPLAY', true );
-define( 'WP_DEBUG', true );
 
 define('AUTH_KEY',         'h{~oo[3h@)Cxn(r23w^3bdRbkvh?@x`eG~VduWJiU0_dt.0SH5[@+CW3F=N%Ij{q');
 define('SECURE_AUTH_KEY',  '&}q.qxmsd[vQ3fe)|-#Ig`<R%L(Jl%BS|I|~(ktM|JJV|~[%~H?u-]C-t?Kft#Dx');
@@ -24,3 +22,5 @@ define('AUTH_SALT',        '[X}CU^hA+-q(|!*[-:6][@aj*KJYgwuH4{L5zu3Jgoh{n5)R/.@{
 define('SECURE_AUTH_SALT', 'HBI&V*%- M2l|8.h|.*qCMz<T@+}8vr1BS,{+*x6dol~BK?K`,stz$ju&-Z=Eod6');
 define('LOGGED_IN_SALT',   'S^Xq-Ge9qm*Z3E5x mGl6i,)n9|<vy7B#JH+V<d^,-tS5MeZeNO/Eq^$u7U5XM+,');
 define('NONCE_SALT',       '$Y&#z-?hJMBMwlW5p0LF=HAGiyK!XFC@+VL37Rj,/O<+<anFH! IKv<Q*KQv=Q+f');
+
+/* That's all, stop editing! Happy blogging. */
---
+++ dploy.sh
@@ -0,0 +1,93 @@
+#!/bin/sh
+
+# Clearing prompt
+clear
+
+echo "
+///////////////////////////////,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,     
+(////////////////@@/////////@@(,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,    
+ ////////////////@@////////(@@/,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,   
+ ////////////////@@/#@@@&//(@@//@@*(@@@&*,,,/&@@@@(,/@@,,,,,(@@,,,,,,,,,,,,,   
+ ////////////////@@@(/(@@@/(@@/(@@@(,,*@@@,,//,,,%@@*%@&,,,/@@,,,,,,,,,,,,,,,  
+  ///////////////@@////(@@/(@@/(@@*,,,,,@@%,,%@@&%@@%,@@&,,@@/,,,,,,,,,,,,,,,  
+  (//////////////@@////(@@/(@@/(@@(,,,,,@@#@@%,,,,@@%,,@@(&@%,,,,,,,,,,,,,,,,, 
+   //////////////@@////(@@/(@@/(@@@@(*%@@@,@@@,,,&@@%,,*@@@&,,,,,,,,,,,,,,,,,, 
+    /////////////@@////(@@/(@@/(@@*,(@@*,,,,,(@@/,((/,,,*@@,,,,,,,,,,,,,,,,,,,,
+    ///////////////////////////(@@*,,,,,,,,,,,,,,,,,,&@@@@,,,,,,,,,,,,,,,,,,,,,
+     //////////////////////////////,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+"
+
+# Duplicate dist config file
+cp dev-config.sample.php dev-config.php
+
+#Current date and time
+NOW=$(date +"%d-%m-%Y-%H-%M-%S")
+
+# Read config
+DB_NAME=wpgit_test_$NOW
+DB_USER=root
+DB_PASSWORD=root
+DB_HOST=localhost
+IP="localhost:8888"
+
+# Read user settings
+read -p "DB_NAME [$DB_NAME]: " CUSTOM_DB_NAME
+CUSTOM_DB_NAME=${CUSTOM_DB_NAME:-$DB_NAME}
+read -p "DB_USER [$DB_USER]: " CUSTOM_DB_USER
+CUSTOM_DB_USER=${CUSTOM_DB_USER:-$DB_USER}
+read -p "DB_PASSWORD [$DB_PASSWORD]: " CUSTOM_DB_PASSWORD
+CUSTOM_DB_PASSWORD=${CUSTOM_DB_PASSWORD:-$DB_PASSWORD}
+read -p "DB_HOST [$DB_HOST]: " CUSTOM_DB_HOST
+CUSTOM_DB_HOST=${CUSTOM_DB_HOST:-$DB_HOST}
+read -p "IP [$IP]: " CUSTOM_IP
+CUSTOM_IP=${CUSTOM_IP:-$IP}
+
+# Define DB config
+echo "\n\ndefine('DB_NAME', '$CUSTOM_DB_NAME');" >> dev-config.php
+echo "define('DB_USER', '$CUSTOM_DB_USER');" >> dev-config.php
+echo "define('DB_PASSWORD', '$CUSTOM_DB_PASSWORD');" >> dev-config.php
+echo "define('DB_HOST', '$CUSTOM_DB_HOST');" >> dev-config.php
+
+# Show DB config
+WPDBNAME=`cat dev-config.php | grep DB_NAME | cut -d \' -f 4`
+WPDBUSER=`cat dev-config.php | grep DB_USER | cut -d \' -f 4`
+WPDBPASS=`cat dev-config.php | grep DB_PASSWORD | cut -d \' -f 4`
+WPDBHOST=`cat dev-config.php | grep DB_HOST | cut -d \' -f 4`
+echo "\nDB_NAME: "$WPDBNAME
+echo "DB_USER: "$WPDBUSER
+echo "DB_PASSWORD: "$WPDBPASS
+echo "DB_HOST: "$WPDBHOST
+echo
+
+# URL Search in dump.sql
+URL_SEARCH=`grep home dump.sql | cut -d \' -f 10`
+echo "URL Search: "$URL_SEARCH
+
+#Full path
+CURRENT_PATH=$PWD
+
+#Current folder (partial path)
+CURRENT_DIRECTORY=${PWD##*/}
+
+# URL Replace
+URL_REPLACE="http://"$CUSTOM_IP"/"$CURRENT_DIRECTORY
+echo "URL Replace: "$URL_REPLACE
+
+#Update wp-config WP_CONTENT_URL
+echo "define( 'WP_CONTENT_URL', '$URL_REPLACE/public/wp-content' );" >> dev-config.php
+
+# WP-Cli
+wp --info
+composer install
+wp db create
+wp db import dump.sql
+wp search-replace $URL_SEARCH $URL_REPLACE
+wp option update home $URL_REPLACE
+wp option update siteurl $URL_REPLACE"/public/wp"
+wp rewrite structure '/%postname%/'
+wp plugin status
+wp theme status
+
+siteurl=$(wp option get siteurl)
+url=$(echo $siteurl | awk -F/ '{print $3}')
+echo $url
